4e3284d59f1c857d06f994bf1ed3be93e9284a59
carmemias <info@carmemias.com>
2019-04-08 11:59:12 +0100

    Added sw registration and static assets precaching

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+// See https://stackoverflow.com/questions/51214220/vue-cli-3-how-to-use-the-official-pwa-plugin-service-worker
+// Also https://www.npmjs.com/package/@vue/cli-plugin-pwa
+// vue.config.js
+module.exports = {
+	// ...other vue-cli plugin options...
+	//for testing production publicPath: '/mkwp/',
+	pwa: {
+		// configure the workbox plugin
+		// workboxPluginMode: 'GenerateSW'
+		workboxPluginMode: 'InjectManifest',
+		workboxOptions: {
+			// swSrc is required in InjectManifest mode.
+			swSrc: 'src/service-worker.js',
+			include: [/\.html$/, /\.png$/, /\.js$/, /\.css$/]
+		}
+			// ...other Workbox options...
+			// runTimeCaching:[{}]
+		}
+	}
